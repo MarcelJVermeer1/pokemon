b2cec49af0e6a9b765ac5fdbb1b49a615636fcd5
MarcelJVermeer1 <150677934+MarcelJVermeer1@users.noreply.github.com>
2025-03-31 13:31:28 +0200

    main page responsive

--- src/app/layout/cards-layout/cards-layout.component.ts
+++ src/app/layout/cards-layout/cards-layout.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, NgFor } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Pokemon} from '../../shared/interfaces/pokemon';
 import { PokemonService } from '../../core/services/pokemon.service';
 import { CardComponent } from '../card/card.component';
@@ -16,16 +16,17 @@ export class CardsLayoutComponent  implements OnInit{
 
   pokemons: Pokemon[] = [];
   currentPage = 0;
-  pageSize = 21;
+  pageSize = 0;
 
   constructor(private pokemonService: PokemonService) { }
-
+  
   public ngOnInit() {
     this.loadPage(this.currentPage);
   }
 
   public loadPage(page: number ) {
     const offset = page * this.pageSize;
+    this.pageSize = this.getResponsivePageSize();
     //get the pokemon list from the service and subscribe to it.
     this.pokemonService.getPokemonList(offset, this.pageSize).subscribe(pokemons => {
       this.pokemons = pokemons;
@@ -43,5 +44,19 @@ export class CardsLayoutComponent  implements OnInit{
       this.loadPage(this.currentPage - 1);
     }
   }
+
+  //D etermines the page size based on the window width.
+  private getResponsivePageSize(): number {
+    const width = window.innerWidth;
+    if (width >= 1600) return 21;
+    if (width >= 1024) return 18; 
+    if (width >= 768) return 12;
+    return 9;                    
+  }
+  @HostListener('window:resize')
+onResize() {
+  this.loadPage(this.currentPage); 
+}
+
 
 }
